cf5e67cad7217f3da2f24b7dfd016e711eddf8b6
Sunny <57601719+whlx228@users.noreply.github.com>
2020-12-16 21:27:42 -0500

    Create query2.sql

---
+++ query2.sql
@@ -0,0 +1,18 @@
+```
+SELECT DISTINCT a.Company,
+         a.Highest_Hourly_Price,
+         ts AS datetime,
+         a.hour
+FROM 
+    (SELECT name AS Company,
+         (EXTRACT(HOUR
+    FROM (CAST(ts AS TIMESTAMP))) - 4) AS hour, max(high) AS Highest_Hourly_Price
+    FROM sta9760f2020yfinance
+    GROUP BY  1, 2
+    ORDER BY  1, 2 ) a, sta9760f2020yfinance b
+WHERE a.Company=b.name
+        AND a.hour = (EXTRACT(HOUR
+FROM (CAST(ts AS TIMESTAMP))) - 4)
+        AND a.Highest_Hourly_Price = b.high
+ORDER BY  1, 4
+```
